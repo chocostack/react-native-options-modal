fc52e99d1b868f20fc3031d7c552aad743c3e1eb
salim_assad_sanchez <salim.assad.sanchez@gmail.com>
2023-08-09 14:08:05 -0600

    1.0.6

--- OptionModal.js
+++ OptionModal.js
@@ -1,6 +1,6 @@
-﻿import React, { useState } from 'react';
+﻿import React, {useState} from 'react';
 
-import { Modal, View, TouchableWithoutFeedback, Text } from 'react-native';
+import {Modal, View, TouchableWithoutFeedback, Text, ScrollView} from 'react-native';
 
 import Radio from './Radio';
 
@@ -13,44 +13,52 @@ const OptionModal = (props) => {
     }
 
     return <Modal
-            transparent={true}
-            visible={props.isVisible}
-            onRequestClose={props.toggleModal}>
+        transparent={true}
+        visible={props.isVisible}
+        onRequestClose={props.toggleModal}
+    >
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.6)'
+        }}>
             <View style={{
-                flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'rgba(0,0,0,0.6)'
+                width: 300,
+                backgroundColor: 'white',
+                borderRadius: 16,
+                padding: 15,
+                paddingBottom: 0
             }}>
-                <View style={{
-                    width: 300,
-                    backgroundColor: 'white',
-                    borderRadius: 16,
-                    padding: 15,
-                    paddingBottom: 0
-                }}>
-                    <Text style={{ fontSize: 20, padding: 7, fontWeight: 'bold', ...props.titleStyles }}>
-                        {props.title}
-                    </Text>
-                    {props.options.map(item => <TouchableWithoutFeedback key={item.id} onPress={() => selectItem(item)}>
-                        <View style={{ padding: 15, flexDirection: 'row' }}>
+                <Text style={{fontSize: 20, padding: 7, fontWeight: 'bold', ...props.titleStyles}}>
+                    {props.title}
+                </Text>
+                <ScrollView
+                    style={{ maxHeight: 400 }}
+                >
+                    {props.options.map(item => <TouchableWithoutFeedback
+                        key={item.id}
+                        onPress={() => selectItem(item)}
+                    >
+                        <View style={{padding: 15, flexDirection: 'row'}}>
                             <Radio
                                 selected={props.selected == item.id}
                                 style={props.radioStyle}
                                 selectedColor={props.selectedColor}
                             />
-                            <View style={{ marginLeft: 15, paddingRight: 25 }}>
-                                <Text style={{ fontSize: 17, ...props.textStyles }}>
+                            <View style={{marginLeft: 15, paddingRight: 25}}>
+                                <Text style={{fontSize: 17, ...props.textStyles}}>
                                     {item.text}
                                 </Text>
                             </View>
                         </View>
                     </TouchableWithoutFeedback>)}
+                </ScrollView>
 
-                </View>
             </View>
-        </Modal>;
+        </View>
+    </Modal>;
 };
 
 export default OptionModal;
